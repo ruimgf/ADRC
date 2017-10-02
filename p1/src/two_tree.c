c048981b2bb9f79b92b24b5374742bc29ab82a20
candeiasalexandre <candeiasalexandre@gmail.com>
2017-10-02 10:09:21 +0100

    started implementation of two bit tree

---
+++ p1/src/two_tree.c
@@ -0,0 +1,8 @@
+#include "two_tree.h"
+
+Node * createTwoTree(){
+  Node* root;
+  root = (Node*)malloc(sizeof(Node));
+  
+
+}
---
+++ p1/src/two_tree.h
@@ -0,0 +1,22 @@
+#ifndef	TWO_TREE_H
+#define	TWO_TREE_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef struct Node{
+  int nextHoop;
+  struct Node * childs[4]; //If we go right it means that adress is 1 for that bit //If we go left it means that address is 0 for that bit
+}Node;
+
+Node * createTwoTree();
+
+int InsertPrefixTwoTree(Node * root , char * address , int nextHoop);
+
+int freeTwoTree(Node * node);
+
+void PrintTableEven(Node * root);
+
+
+#endif
